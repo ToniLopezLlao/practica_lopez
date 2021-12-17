eb398c8608159b561b54c9008a0f39cd47503169
Toni López Llaó <tonilopez450@gmail.com>
2021-12-17 12:26:03 +0100

    w

--- practica_lopez/src/practica_lopez/Practica_lopez.java
+++ practica_lopez/src/practica_lopez/Practica_lopez.java
@@ -2,8 +2,20 @@ package practica_lopez;
 
 public class Practica_lopez {
 
+
+		public static void main(String[] args) {
+			printOne();
+			printOne();
+			
+			
+		}
+		public static void printOne() {
+			System.out.println("Hello World");
+		}
+
+	
 	public static void printThree() {
 		System.out.println("Hello World");
 	}
-
 }
+
